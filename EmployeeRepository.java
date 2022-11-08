aaa11d21b41f394b88bef3bc37de0ee4c087be2e
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-11-08 17:00:32 +0530

    Add files via upload

---
+++ EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+	Optional <Employee> findByName(String name);
+
+}
