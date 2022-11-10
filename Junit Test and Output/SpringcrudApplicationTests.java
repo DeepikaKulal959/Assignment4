722f5676b5ddfebac71042d416b10b2c30966290
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-11-10 14:27:56 +0530

    SpringcrudApplicationTests.java

---
+++ Junit Test and Output/SpringcrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springcrud;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
