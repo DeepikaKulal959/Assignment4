be1f25c0a23b5fd5de7f7c28f6f2cbc8d10894b4
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-11-14 09:59:35 +0530

    Update EmployeeTest.java

--- Junit Test and Output/EmployeeTest.java
+++ Junit Test and Output/EmployeeTest.java
@@ -5,21 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
-    /**
-     * Methods under test:
-     *
-     * <ul>
-     *   <li>default or parameterless constructor of {@link Employee}
-     *   <li>{@link Employee#setAge(int)}
-     *   <li>{@link Employee#setId(int)}
-     *   <li>{@link Employee#setName(String)}
-     *   <li>{@link Employee#setStatus(String)}
-     *   <li>{@link Employee#getAge()}
-     *   <li>{@link Employee#getId()}
-     *   <li>{@link Employee#getName()}
-     *   <li>{@link Employee#getStatus()}
-     * </ul>
-     */
+    
     @Test
     void testConstructor() {
         // Arrange and Act
